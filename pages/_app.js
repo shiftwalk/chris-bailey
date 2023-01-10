b38ae7b937fa7d6185f4eb3a6dfc44e43c71cae6
Sam Goddard <hello@samgoddard.co.uk>
2023-01-10 14:59:48 +0000

    date fixed

--- pages/_app.js
+++ pages/_app.js
@@ -56,7 +56,7 @@ export default function App({ Component, pageProps }) {
       { pageProps.preview && (<div className={'fixed bottom-0 left-0 w-auto px-3 py-2 bg-red-600 text-white justify-center flex z-[200] uppercase font-mono text-sm m-3'}>Preview Mode - <a className={'px-1 underline'} href={`/api/exit-preview?currentRoute=${router.route}`}>Click To Exit</a></div>)}
       
       <IntroContext.Provider value={[introContext, setIntroContext]}>
-        <div className="hidden lg:block">
+        <div className="block">
           <LazyMotion features={domAnimation}>
           { !introContext && router.asPath == '/' && (
               <m.div 
