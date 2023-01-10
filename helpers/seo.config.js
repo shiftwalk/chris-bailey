4cb88694e8a69fdf34e103f57d6dd70e6aa87754
Sam Goddard <hello@samgoddard.co.uk>
2023-01-10 12:11:22 +0000

    date fixed

--- helpers/seo.config.js
+++ helpers/seo.config.js
@@ -1,18 +1,18 @@
 export default {
-  defaultTitle: 'Designer, Art Director | Chris Bailey',
+  defaultTitle: 'Designer, Art Director | Christopher Bailey',
   description: 'As a successful art director, I build strong, timeless and flexible brand identities by bringing together imagination, technical skill and keen business awareness.',
-  titleTemplate: '%s | Chris Bailey',
+  titleTemplate: '%s | Christopher Bailey',
   openGraph: {
     type: 'website',
     locale: 'en_GB',
-    url: 'https://chris-bailey.vercel.app/',
-    site_name: 'Chris Bailey',
+    url: 'https://cpbailey.co.uk/',
+    site_name: 'Christopher Bailey',
     images: [
       {
         url: '/images/social.jpg',
         width: 1200,
         height: 675,
-        alt: `Chris Bailey`,
+        alt: `Christopher Bailey`,
       },
     ],
   },
